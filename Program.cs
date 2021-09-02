1229dc6770c0b9b93d500568c33a1ecebdc4b7e1
beeman_lucas <beeman_lucas@G-02-20>
2021-09-02 10:01:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int goldValue = 10;
+            const int silverValue = 5;
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.Write("Enter an amount of money in cents: ");
+            string valueString = Console.ReadLine();
+            double value = Convert.ToDouble(valueString);
+            double gold = Math.Floor(value / 10);
+            double remainder = value % goldValue;
+            double silver = Math.Floor(remainder / 5);
+            remainder = remainder % silverValue;
+            Console.WriteLine($"{value} is equal to: {gold} gold, {silver} silver, and {remainder} bronze.");
+        }
+    }
+}
